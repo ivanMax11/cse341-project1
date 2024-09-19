66fd7660dc1cb9d16fd7f28e38e45268419e5298
Iván Maximiliano Monte <ivanmonte07.93@gmail.com>
2024-09-19 13:32:14 -0300

    initial checking for lesson1

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use('/', require('./routes'));
+
+app.listen(port, () => {console.log(`Running in port ${port}`)});
